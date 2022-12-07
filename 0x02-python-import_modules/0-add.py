28d2be02884a0e608c8237e7c8b423767296ed4b
joseph thuku <jthuku490@gmail.com>
2022-12-07 11:49:51 +0300

    i

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 
-from add_0 import add
+if __name__ == "__main__":
+    """Print some of 1 and 2"""
+    from add_0 import add
 
-result = add(1, 2)
-print(f"1 + 2 = {result}")
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
