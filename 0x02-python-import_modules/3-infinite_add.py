e02940c174daa4811f471ba141d98fb24822510d
joseph thuku <jthuku490@gmail.com>
2022-12-07 13:00:49 +0300

    fixing

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,13 @@
-#!/usr/bin/python3
-if __name__ == "__main__":
-    import sys
-    result = 0
-    for arg in sys.argv:
-        if arg != sys.argv[0]:
-            result += int(arg)
-    print(result)
+#!/usr/bin/env python3
+
+import sys
+
+# Initialize the result variable
+result = 0
+
+# Iterate over the arguments and add them to the result
+for arg in sys.argv[1:]:
+    result += int(arg)
+
+# Print the result
+print(result)
