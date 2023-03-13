0b88c4f1cd55f7c312790ec83915c768d353ea8c
josephthuku <jthuku490@gmail.com>
2023-03-13 19:09:09 +0000

    argv is much

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const args = process.argv.slice(2);
+if (args[0] && args[1]) {
+  console.log(`${args[0]} is ${args[1]}`);
+} else {
+  console.log('Usage: node script.js <arg1> <arg2>');
+}
