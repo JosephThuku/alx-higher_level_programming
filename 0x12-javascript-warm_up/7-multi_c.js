664fc9b10d6c2e6f5f35a3917f27e23e7234602b
josephthuku <jthuku490@gmail.com>
2023-03-13 21:08:04 +0000

    debugging

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-let x = 1;
+let x = 0;
 const args = process.argv.slice(2);
 if (isNaN(args[0])) {
   console.log('Missing number of occurrences');
 } else {
-  while (x <= args[0]) {
+  while (x < parseInt(args[0])){
     console.log('c is fun');
     x++;
   }
