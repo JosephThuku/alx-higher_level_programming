bcca3e7ed8f8c1adeb8330c90190348ed07249b2
josephthuku <jthuku490@gmail.com>
2023-03-14 05:59:40 +0000

    debugged the square output

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
-if (isNaN(process.argv[2])) {
+
+if (isNaN(Number(process.argv[2]))) {
   console.log('Missing size');
 } else {
-  for (let i = 0; i <= process.argv[2]; i++) {
-    console.log('x'.repeat(process.argv[2]));
+  for (let i = 0; i < process.argv[2]; i++) {
+    console.log('X'.repeat(process.argv[2]));
   }
 }
