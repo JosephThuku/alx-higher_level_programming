b2e20912461c024927dc41323b07662b231462dd
JosephThuku <jthuku490@gmail.com>
2023-03-18 12:07:38 -0700

    debugged the filter to first()

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -15,6 +15,9 @@ if __name__ == "__main__":
             pool_pre_ping=True)
     Base.metadata.create_all(engine)
     session = Session(engine)
-    result = session.query(State).filter(State.id == 1).all()
-    for state in result:
-        print(f"{state.id}: {state.name}")
+    result = session.query(State).first()
+    if (State):
+        print(f"{result.id}: {result.name}")
+    else:
+        print("Nothing")
+    session.close()
