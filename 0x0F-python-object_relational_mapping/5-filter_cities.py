33ec603590c375b441b39a48d9194d2610d7c0bf
JosephThuku <jthuku490@gmail.com>
2023-03-18 06:50:32 -0700

    added main

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,21 +1,33 @@
 #!/usr/bin/python3
 """
-Python script that lists cities by states
+script that takes in the name of a state as an argument
+and lists all cities of that state, using the database hbtn_0e_4_usa
 """
 import MySQLdb
-import sys
+from sys import argv
 
-state_name = sys.argv[4]
-db = MySQLdb.connect(
-        host="localhost",
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3],
-        port=3306)
-cur = db.cursor()
-cur.execute("SELECT cities.name FROM cities JOIN states ON cities.state_id = states.id WHERE states.name=%s ORDER BY cities.id", (state_name,))
-rows = cur.fetchall()
-for row in rows:
-    print(row)
-cur.close()
-db.close()
+
+def main():
+    conn = MySQLdb.connect(host="localhost",
+                           port=3306,
+                           user=argv[1],
+                           passwd=argv[2],
+                           db=argv[3],
+                           charset="utf8")
+    cursor = conn.cursor()
+    sql_cmd = """
+    SELECT cities.name
+    FROM cities
+    JOIN states
+    ON cities.state_id = states.id
+    WHERE states.name LIKE %s
+    ORDER BY cities.id ASC
+    """
+    cursor.execute(sql_cmd, (argv[4], ))
+    print(', '.join(["{:s}".format(row[0]) for row in cursor.fetchall()]))
+    cursor.close()
+    conn.close()
+
+
+if __name__ == "__main__":
+    main()
