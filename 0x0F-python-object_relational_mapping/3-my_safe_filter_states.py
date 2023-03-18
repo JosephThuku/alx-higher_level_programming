b458af5d9e76da9b13a43e6c8e980fa9db6e7870
JosephThuku <jthuku490@gmail.com>
2023-03-18 06:42:38 -0700

    introduced functionmain and debugged indention errors

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -5,17 +5,25 @@ Python script that lists all states from database hbtn_0e_usa
 import MySQLdb
 import sys
 
-input = sys.argv[4]
-db = MySQLdb.connect(
+def main():
+    input = sys.argv[4]
+    db = MySQLdb.connect(
         host="localhost",
         user=sys.argv[1],
         passwd=sys.argv[2],
         db=sys.argv[3],
         port=3306)
-cur = db.cursor()
-cur.execute("SELECT * FROM states WHERE name=%s ORDER BY states.id", (input,))
-rows = cur.fetchall()
-for row in rows:
-    print(row)
-cur.close()
-db.close()
+    cur = db.cursor()
+    cur.execute("""
+    SELECT *FROM states
+    WHERE name=%s
+    ORDER BY states.id""", (input,))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
+
+
+if __name__ == "__main___":
+    main()
