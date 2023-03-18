413f6f3b30a17c683dbf3c39e214e0229371d16c
JosephThuku <jthuku490@gmail.com>
2023-03-18 05:38:21 -0700

    debugged the for loop

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,20 +1,29 @@
 #!/usr/bin/python3
-"""
-Python script that lists all cities from database hbtn_0e_4_usa
-"""
-import MySQLdb
-import sys
 
-db = MySQLdb.connect(
-        host="localhost",
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3],
-        port=3306)
-cur = db.cursor()
-cur.execute("SELECT * FROM cities ORDER BY cities.id ASC")
-rows = cur.fetchall()
-for row in rows:
-    print(row)
-cur.close()
-db.close()
+""" This module lists all the cities from the database
+hbtn_0e_4_usa
+"""
+
+if __name__ == '__main__':
+    import MySQLdb
+    import sys
+
+    db = MySQLdb.connect(host='localhost',
+                         port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
+    cur = db.cursor()
+
+    cur.execute("SELECT cities.id, cities.name, states.name\
+                FROM cities\
+                INNER JOIN states\
+                ON cities.state_id = states.id\
+                ORDER BY cities.id")
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
