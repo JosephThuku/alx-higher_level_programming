f1579c8c322738b4556d0c5138531b7fe6b53daa
JosephThuku <jthuku490@gmail.com>
2023-03-18 06:11:51 -0700

    introduced main function

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -7,14 +7,26 @@ from the database hbtn_0e_0_usa
 import MySQLdb
 import sys
 
-conn = MySQLdb.connect(
+
+def main():
+    conn = MySQLdb.connect(
         host="localhost",
         user=sys.argv[1],
         passwd=sys.argv[2],
         db=sys.argv[3],
         port=3306)
-cur = conn.cursor()
-cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC")
-rows = cur.fetchall()
-for row in rows:
-    print(row)
+    cur = conn.cursor()
+    cur.execute("""
+    SELECT * FROM states
+    WHERE name LIKE 'N%'
+    ORDER BY states.id ASC""")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+        cur.close()
+        conn.close()
+
+
+if __name__ == "__main__":
+    main()
