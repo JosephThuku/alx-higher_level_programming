a927be09fa156e68f3a7f717ab3b32e90143adf2
JosephThuku <jthuku490@gmail.com>
2023-03-31 12:03:15 -0700

    oops forgot to ensure file cant execute when imported

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -4,6 +4,8 @@ script to print the x-request-id
 """
 import urllib.request
 import sys
-with urllib.request.urlopen(sys.argv[1]) as response:
-    headers = dict(response.info())
-    print(headers.get("X-Request-Id"))
+if __name__ == "__main__":
+    url = sys.argv[1]
+    with urllib.request.urlopen(url) as response:
+        headers = dict(response.info())
+        print(headers.get("X-Request-Id"))
