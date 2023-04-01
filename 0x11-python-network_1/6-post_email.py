1d7f3b2aebcc6778c0e1982c52b03371c9a25640
JosephThuku <jthuku490@gmail.com>
2023-03-31 23:55:46 -0700

    fixed missing white space

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -7,6 +7,6 @@ import requests
 import sys
 if __name__ == "__main__":
     myurl = sys.argv[1]
-    mail = {"email":sys.argv[2]}
+    mail = {"email": sys.argv[2]}
     r = requests.post(myurl, data=mail)
     print(r.text)
