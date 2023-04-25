bc8efe24ad3c85e45e56064f2dd4700f21f7e6c3
JosephThuku <jthuku490@gmail.com>
2023-04-25 14:37:33 +0000

    status code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -7,6 +7,6 @@ request.get(url, (error, response) => {
   if (error) {
     console.error('Error:', error);
   } else {
-    console.log('Code:', response.statusCode);
+    console.log('code:', response.statusCode);
   }
 });
