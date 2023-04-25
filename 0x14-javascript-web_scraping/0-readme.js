0bd2e0f199ff11a1494bcb11fd78e7e8daeb1093
JosephThuku <jthuku490@gmail.com>
2023-04-25 09:30:44 +0000

    0. Readme

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const path = process.argv[2];
 const fs = require('fs');
-const text = fs.readFileSync(path,"utf-8");
-console.log(text)
+const text = fs.readFileSync(path, 'utf-8');
+console.log(text);
